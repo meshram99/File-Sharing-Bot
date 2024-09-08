ac62a3da902edd2df0bd5eb51fbe3fcac4bb4bb2
meshram99 <instagrampage9284@gmail.com>
2024-09-08 12:20:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,16 +13,16 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("29895250", ""))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("29ca1e2311efdf950eea03a6ae2bc8ee", "")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("2017335429", ""))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
